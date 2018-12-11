c6b3d029574d7aa83fac330dd4b9439d103266a3
Martin Raifer <martin.raifer@uni-heidelberg.de>
2018-12-11 14:32:49 +0100

    fix navbar stuck open issue, add link to newsletter to navbar

--- js/navbar-collapse.js
+++ js/navbar-collapse.js
@@ -1,17 +1,13 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+var navbarCollapse = function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+}
+$(window).scroll(navbarCollapse);
+$(document).ready(navbarCollapse);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -28,5 +24,3 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-function init() {}
